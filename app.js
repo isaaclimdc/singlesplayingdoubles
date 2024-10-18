57ed7f005baabf2dc7b22ae18e9062ba77b3f4d1
Isaac Lim <isaaclimdc@gmail.com>
2024-10-18 20:46:08 +0800

    Form changes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mysql = require('mysql2');
 const bodyParser = require('body-parser');
+// const nodemailer = require('nodemailer');
 const path = require('path');
 
 const app = express();
@@ -27,6 +28,15 @@ db.connect((err) => {
 // Middleware for parsing form data
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// // Nodemailer setup (Gmail example)
+// const transporter = nodemailer.createTransport({
+//     service: 'gmail', // e.g., Gmail (you can use any SMTP service provider)
+//     auth: {
+//         user: '', // your email address
+//         pass: '', // your email password (use app password for Gmail)
+//     },
+// });
+
 // Middleware to serve static files (CSS, images, JS, etc.)
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -43,14 +53,17 @@ app.get('/thanks', (req, res) => {
 
 // Handle form submission
 app.post('/register', (req, res) => {
-    const { name, email, age, gender, ig_handle, skill, notes } = req.body;
+    const { name, age, gender, email, phone, ig_handle, tennis_level, personality_notes, misc_notes } = req.body;
 
     const ig_handle_clean = String(ig_handle).replace('@', '');
     const createdAt = new Date();
 
-    const sql = `INSERT INTO applications (name, email, age, gender, ig_handle, skill, notes, created_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`;
+    const sql = `INSERT INTO applications_v1
+                    (name, age, gender, email, phone, ig_handle, tennis_level, personality_notes, misc_notes, created_at)
+                VALUES
+                    (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
 
-    db.query(sql, [name, email, age, gender, ig_handle_clean, skill, notes, createdAt], (err, result) => {
+    db.query(sql, [name, age, gender, email, phone, ig_handle_clean, tennis_level, personality_notes, misc_notes, createdAt], (err, result) => {
         if (err) {
             console.error('Error inserting data:', err);
             res.status(500).send('An error occurred while processing your registration.');
@@ -59,6 +72,23 @@ app.post('/register', (req, res) => {
         }
     });
 });
+
+// function sendEmail(email, name) {
+//     const mailOptions = {
+//         from: '',
+//         to: email, // recipient email address (you can also send to a static address or admin)
+//         subject: 'Registration Successful',
+//         text: `${name} registered for Singles Playing Doubles`,
+//     };
+
+//     transporter.sendMail(mailOptions, (error, info) => {
+//         if (error) {
+//             console.error('Error sending email:', error);
+//         } else {
+//             console.log('Email sent:', info.response);
+//         }
+//     });
+// }
 
 // Start the server
 app.listen(3000, () => {
